6da2cf998c34873a1ec8da8e126b6ad9c0399e69
jiaminglei <jiaminglei@blacklake.cn>
2023-01-13 09:42:34 +0800

    配置webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const appDirectory = path.resolve(__dirname, './');
+const appDirectory = path.resolve(__dirname, '../');
 
 const tsLoaderConfiguration = {
   test: /\.tsx?$/,
@@ -20,14 +20,19 @@ const babelLoaderConfiguration = {
   include: [
     path.resolve(appDirectory, 'index.web.js'),
     path.resolve(appDirectory, 'src'),
-    path.resolve(appDirectory, 'node_modules/react-native-uncompiled')
   ],
   use: {
     loader: 'babel-loader',
     options: {
       cacheDirectory: true,
       // The 'metro-react-native-babel-preset' preset is recommended to match React Native's packager
-      presets: ['module:metro-react-native-babel-preset'],
+      presets: [
+        'module:metro-react-native-babel-preset',
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+        '@babel/preset-flow',
+      ],
       // Re-write paths to import only the modules needed by the app
       plugins: ['react-native-web'],
     },
